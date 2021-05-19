eba3c84f45b32641f54eed52e9ec0cc552022e35
J-R0101 <jrjose@outlook.com>
2021-05-19 09:00:45 -0300

    cursoweb

---
+++ Fundamentos_JS/hosting.js
@@ -0,0 +1,22 @@
+console.log('a =', a);
+var a = 2;
+console.log('a =', a);
+
+
+console.log('b =', b);
+var b = 2;
+console.log('b =', b);
+
+
+function teste() {
+    console.log('c =', c);
+    var c = 2;
+    console.log('c =', a);
+}
+
+teste();
+
+
+//console.log('d =', d); gera erro
+let d = 2;
+console.log('d =', d);
