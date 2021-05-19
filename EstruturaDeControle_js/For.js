2a41c740e87ae1d055959cf96d0c971a10605a5d
J-R0101 <jrjose@outlook.com>
2021-05-19 13:28:46 -0300

    cursoweb

---
+++ EstruturaDeControle_js/For.js
@@ -0,0 +1,12 @@
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`i = ${i}`);
+    
+}
+
+const notas = [6.7, 7, 10, 8.1, 7.7]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`notas = ${notas[i]}`);
+
+}
