a9a563aa3c0734000f35a94ff6a15c58480d0914
J-R0101 <jrjose@outlook.com>
2021-05-18 23:54:23 -0300

    cursoweb

---
+++ Fundamentos/Comentarios.js
@@ -0,0 +1,4 @@
+//Comentario de uma linha
+console.log("linha 1");
+/*Comentario de multiplas linhas */
+console.log("Linha 2");
---
+++ Fundamentos/Strings.js
@@ -0,0 +1,20 @@
+const escola = "Cod3r";
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(5));
+console.log(escola.charCodeAt(3)); // valor na tabela ask/unicode da silaba de indice 3
+console.log(escola.indexOf('3'));//valor assosiado ao indice 3
+
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log('Escola' .concat(escola).concat("!"));
+
+console.log(escola.replace(3, 'e'));
+console.log(escola.replace(/\d/, 'e'));
+
+console.log('Ana,Maria,Pedro'.split(','));
+console.log('Ana,Maria,Pedro'.split(/,/));
+
+
+
