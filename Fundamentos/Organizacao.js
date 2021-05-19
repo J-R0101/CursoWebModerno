457443fc7c4edef0dcfe63a70c15235bc732afa3
J-R0101 <jrjose@outlook.com>
2021-05-18 22:44:24 -0300

    cursoweb

---
+++ Fundamentos/Organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de codigo");
+
+//blocos de codigo vazio
+{
+    {
+        // pode ou não terminar com ponto e virgula
+        console.log("Olá");
+        console.log('Mundo')
+    }
+}
