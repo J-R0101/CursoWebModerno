be02df22f782d607f6305cbd25c56460922e7569
J-R0101 <jrjose@outlook.com>
2021-05-20 11:50:12 -0300

    cursoweb

---
+++ OO_js/Oo.js
@@ -0,0 +1,23 @@
+// Codigo não Executavel
+
+// Procedural
+Processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+   valor1,
+   valor2,
+   valor3,
+   Processamento() {
+       // ...
+   }
+};
+
+objeto.Processamento(); // Foco passou a ser o objeto
+
+// Principios Importantes
+
+// 1-Abstração
+// 2-Encapsulamento
+// 3-Herança (Prototype)
+// 4-Polimorfismo
